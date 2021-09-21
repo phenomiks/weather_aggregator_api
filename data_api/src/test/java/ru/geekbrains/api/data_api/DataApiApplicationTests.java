a9dd7c717144d9ad4bffc1bcf9a56a1d9dffa502
phenomiks <phenomikspc@gmail.com>
2021-09-21 13:41:19 +0300

    feat: empty project

---
+++ data_api/src/test/java/ru/geekbrains/api/data_api/DataApiApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.geekbrains.api.data_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
