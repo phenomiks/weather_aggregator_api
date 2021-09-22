d7db8cae64beadf8884eb56da30398e5c6de74e1
phenomiks <phenomikspc@gmail.com>
2021-09-22 16:18:51 +0300

    feat: added empty dispatcher project

---
+++ dispatcher/src/test/java/ru/geekbrains/api/dispatcher/DispatcherApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.geekbrains.api.dispatcher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DispatcherApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
