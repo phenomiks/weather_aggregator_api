ff290b9b23b3b88a359cdc8edad4d189634af14d
phenomiks <phenomikspc@gmail.com>
2021-09-22 16:26:06 +0300

    feat: added empty Loader API project

---
+++ loader_api/src/main/java/ru/geekbrains/api/loader_api/LoaderApiApplication.java
@@ -0,0 +1,13 @@
+package ru.geekbrains.api.loader_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoaderApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LoaderApiApplication.class, args);
+    }
+
+}
---
+++ loader_api/src/test/java/ru/geekbrains/api/loader_api/LoaderApiApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.geekbrains.api.loader_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoaderApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
