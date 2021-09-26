fedb58fea9ff05634f5aa6d19bdd45125362a6d7
phenomiks <phenomikspc@gmail.com>
2021-09-26 15:02:27 +0300

    feat: added users and tokens table schemas

---
+++ auth_api/src/main/resources/db/migration/V1__create.sql
@@ -0,0 +1,22 @@
+BEGIN TRANSACTION;
+
+CREATE TABLE users
+(
+    id         BIGSERIAL PRIMARY KEY,
+    login      VARCHAR(30) NOT NULL UNIQUE,
+    email      VARCHAR(40) NOT NULL UNIQUE,
+    password   VARCHAR(64) NOT NULL,
+    archive    BOOLEAN   DEFAULT FALSE,
+    created_at TIMESTAMP DEFAULT current_timestamp,
+    updated_at TIMESTAMP DEFAULT current_timestamp
+);
+
+CREATE TABLE tokens
+(
+    id         BIGSERIAL PRIMARY KEY,
+    user_id    BIGINT REFERENCES users (id),
+    token      VARCHAR   NOT NULL UNIQUE,
+    created_at TIMESTAMP NOT NULL
+);
+
+COMMIT;
