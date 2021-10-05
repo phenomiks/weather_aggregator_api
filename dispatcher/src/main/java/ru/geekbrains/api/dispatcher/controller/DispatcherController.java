6e49fcf200e61f16cdc01410d10a6d92fc6cb0ad
VAR-VAR <hrom9090@mail.ru>
2021-10-05 16:02:33 +0300

    feat: added interface for DispatcherController

--- dispatcher/src/main/java/ru/geekbrains/api/dispatcher/controller/DispatcherController.java
+++ dispatcher/src/main/java/ru/geekbrains/api/dispatcher/controller/DispatcherController.java
@@ -2,20 +2,18 @@ package ru.geekbrains.api.dispatcher.controller;
 
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import org.springframework.http.*;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.HttpStatusCodeException;
 import ru.geekbrains.api.dispatcher.application.utils.ExceptionHandlerForPostRequester;
 import ru.geekbrains.api.dispatcher.application.utils.ValidateRequestUtils;
+import ru.geekbrains.api.dispatcher.controller.interfaces.DispatcherAbstractController;
 import ru.geekbrains.api.dispatcher.service.AuthService;
 import ru.geekbrains.api.dispatcher.service.DataService;
 
 
 @RestController
-@RequestMapping(value = "/api/v1/dispatcher")
-public class DispatcherController {
+public class DispatcherController implements DispatcherAbstractController {
     private final ValidateRequestUtils validateRequestUtils;
     private final AuthService authService;
     private final DataService dataService;
@@ -27,7 +25,7 @@ public class DispatcherController {
     }
 
     //    http://localhost:8090/api/v1/dispatcher/register
-    @PostMapping(value = "/register")
+    @Override
     public ResponseEntity<?> registerUser(@RequestBody ObjectNode json) {
         try {
             return authService.registerUser(json);
@@ -37,7 +35,7 @@ public class DispatcherController {
     }
 
     //    http://localhost:8090/api/v1/dispatcher/get-weather
-    @PostMapping(value = "/get-weather")
+    @Override
     public ResponseEntity<?> getWeather(@RequestBody ObjectNode json) {
         validateRequestUtils.validateUserRegistrationParameters(json);
         try {
