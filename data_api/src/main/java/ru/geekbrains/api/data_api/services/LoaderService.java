19f84a5c0631a6d7f3d50c8668ffd13f7b14b8b6
phenomiks <phenomikspc@gmail.com>
2021-10-19 11:37:34 +0300

    feat: added a short response to the OpenWeather

--- data_api/src/main/java/ru/geekbrains/api/data_api/services/LoaderService.java
+++ data_api/src/main/java/ru/geekbrains/api/data_api/services/LoaderService.java
@@ -5,15 +5,18 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import ru.geekbrains.api.data_api.application.exception.DataApiException;
 import ru.geekbrains.api.data_api.application.exception.ErrorCode;
+import ru.geekbrains.api.data_api.application.utils.JsonResponseGenerator;
+import ru.geekbrains.api.data_api.model.WeatherService;
+import ru.geekbrains.api.data_api.model.response.ReportResponse;
 import ru.geekbrains.api.data_api.request.DataParameters;
 
 @Service
 public class LoaderService {
-
     @Value("${loader.getWeather}")
     private String url;
 
@@ -33,6 +36,61 @@ public class LoaderService {
                     ErrorCode.CONNECTION_REFUSED,
                     "Connection time out");
         }
-        return responseEntity;
+
+        return checkResponseStatus(responseEntity);
+    }
+
+    private ResponseEntity<ObjectNode> checkResponseStatus(ResponseEntity<ObjectNode> response) {
+        if (response.getStatusCode() != HttpStatus.OK) {
+            return response;
+        }
+
+        ObjectNode responseBody = response.getBody();
+        if (responseBody != null && responseBody.has("status")) {
+            String status = responseBody.path("status").textValue();
+            if (!status.equals(ReportResponse.REPORT_STATUS)) {
+                return response;
+            }
+
+            return prepareShortWeatherResponse(responseBody);
+        }
+
+        return response;
+    }
+
+    private ResponseEntity<ObjectNode> prepareShortWeatherResponse(ObjectNode responseBody) {
+        ObjectMapper mapper = new ObjectMapper();
+        ObjectNode body = mapper.createObjectNode();
+
+        JsonNode responseReport = responseBody.path(ReportResponse.REPORT_STATUS);
+        if (responseReport.has(WeatherService.OPEN_WEATHER.getName())) {
+            boolean isNullOpenWeather = responseReport.path(WeatherService.OPEN_WEATHER.getName()).isNull();
+            if (!isNullOpenWeather) {
+                JsonNode openWeather = responseReport.path(WeatherService.OPEN_WEATHER.getName());
+                ObjectNode node = mapper.createObjectNode();
+                JsonNode current = openWeather.path("current");
+                JsonNode daily = openWeather.path("daily");
+                node.set("current", current);
+                node.set("daily", daily);
+
+                body.set(WeatherService.OPEN_WEATHER.getName(), node);
+            }
+        }
+
+        if (responseReport.has(WeatherService.YANDEX_WEATHER.getName())) {
+            boolean isNullYandexWeather = responseReport.path(WeatherService.YANDEX_WEATHER.getName()).isNull();
+            if (!isNullYandexWeather) {
+                JsonNode yandexWeather = responseReport.path(WeatherService.YANDEX_WEATHER.getName());
+
+                body.set(WeatherService.YANDEX_WEATHER.getName(), yandexWeather);
+            }
+        }
+
+        if (body.isEmpty()) {
+            throw new DataApiException(ErrorCode.INTERNAL_ERROR, "");
+        }
+
+        ObjectNode report = JsonResponseGenerator.generateReportResponseJson(body);
+        return ResponseEntity.ok(report);
     }
 }
