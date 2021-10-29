8a416d5de59bafcdc3237c6ee754f99472bc06e2
Igor <78314860+illa911@users.noreply.github.com>
2021-10-29 19:56:24 +0500

    feet: added a choice of full or short weather

--- data_api/src/main/java/ru/geekbrains/api/data_api/model/request/DataParameters.java
+++ data_api/src/main/java/ru/geekbrains/api/data_api/model/request/DataParameters.java
@@ -5,13 +5,17 @@ import java.util.ArrayList;
 public class DataParameters {
     private final String city;
     private final ArrayList<String> services;
+    private final boolean needDetailed;
 
-    public DataParameters(String city, ArrayList<String> services) {
+    public DataParameters(String city, ArrayList<String> services, boolean needDetailed) {
         this.city = city;
         this.services = services;
+        this.needDetailed = needDetailed;
     }
 
     public String getCity() { return city; }
 
     public ArrayList<String> getServices() { return services; }
+
+    public boolean needDetailed() { return needDetailed; }
 }
