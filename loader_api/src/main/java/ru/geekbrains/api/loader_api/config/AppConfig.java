275ded6fe0fe8c26b6556a15d9837897218aa792
davydov <railagg@gmail.com>
2021-11-06 23:37:17 +0500

    feat: add swagger and Dockerfile

--- loader_api/src/main/java/ru/geekbrains/api/loader_api/config/AppConfig.java
+++ loader_api/src/main/java/ru/geekbrains/api/loader_api/config/AppConfig.java
@@ -10,10 +10,19 @@ import org.springframework.cache.concurrent.ConcurrentMapCacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.DependsOn;
+import org.springframework.web.bind.annotation.RestController;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import java.time.Duration;
 
 @Configuration
+@EnableSwagger2
 @EnableCaching
 public class AppConfig {
 
@@ -47,5 +56,23 @@ public class AppConfig {
         LOGGER.info(BEAN_INITIALIZING + cacheManager);
         LOGGER.info("Cache managers names: " + cacheManager.getCacheNames());
         return cacheManager;
+    }
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.withClassAnnotation(RestController.class))
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(apiInfo());
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("Auth Api Documentation")
+                .description("Documentation of Auth API module included in Weather Aggregator API project")
+                .version("1.0")
+                .build();
     }
 }
